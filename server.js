338b1091ef31af4f841837e42a480cfcb93a38a2
Andy Wu <awpats12@gmail.com>
2023-03-16 16:50:38 -0400

    Basic express routing setup

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-import http from 'http';
+import express from 'express';
 
+const app = express();
 const PORT = 3000;
-
-const server = http.createServer()
 
 const friends = [
   {
@@ -11,42 +10,24 @@ const friends = [
   },
   {
     id: 1,
-    name: 'uw ydna'
+    name: 'happy'
   }
 ]
-server.on('request', (req, res) => {
-  const items = req.url.split('/');
-  // /friends/2 => ['', 'friends', '2']
-  if(req.method === 'POST' && items[1] == 'friends') {
-    req.on('data', (data) => {
-      const friend = data.toString();
-      console.log('Request:', friend);
-      friends.push(JSON.parse(friend));
-      /*
-       Since req is readable stream and res is writable stream,
-       we can just pipe req data back to client through res
-      */
-    })
-    req.pipe(res);
-    
-  }
-  if(req.url === '/friends') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
- 
-    res.end(JSON.stringify({
-      id: 1,
-      name: 'Andy Wu'
-    }));
-  } else if(req.url === '/messages') {
-    res.setHeader('Content-Type', 'application/html')
-    
-  } else {
-    res.statusCode = 404;
-    res.end();
-  }
+app.get('/', (req, res) => {
+  res.send({
+    id: 1,
+    name: 'Andy Wu'
+  });
+})
+
+app.get('/message', (req, res) => {
+  res.send('<ul><li>HELLOOO</li></u l>');
 });
 
-server.listen(PORT, () => {
-  console.log(`Listening on localhost://${PORT}`);
-});
+app.post('/messages', (req, res) => {
+  console.log('updating messages...');
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`)
+})
