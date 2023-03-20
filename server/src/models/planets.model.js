a93bffe658ce2f2e9fb8345b30bdfa966587dd19
Andy Wu <awpats12@gmail.com>
2023-03-19 23:38:04 -0400

    Planet data loaded on server start

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,45 @@
-const planets = [];
+import { parse } from 'csv-parse';
+import fs from 'fs';
 
-export { planets };
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6;
+}
+
+
+function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+      .pipe(parse({
+        comment: '#',
+        columns: true,
+      }))
+      .on('data', (data) => {
+        if (isHabitablePlanet(data)) {
+          habitablePlanets.push(data);
+        }
+      })
+      .on('error', (err) => {
+        console.log(err);
+        reject(err);
+      })
+      .on('end', () => {
+        console.log(`${habitablePlanets.length} habitable planets found!`);
+        resolve();
+      });
+  });
+}
+
+export {
+  habitablePlanets as planets,
+  loadPlanetsData
+}
--- server/src/server.js
+++ server/src/server.js
@@ -2,10 +2,18 @@ import http from 'http';
 
 import app from './app.js';
 
+import { loadPlanetsData } from './models/planets.model.js';
+
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`)
-});
+async function startServer() {
+  await loadPlanetsData();
+
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`)
+  });
+}
+
+startServer();
