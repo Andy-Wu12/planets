0bdb4e642b861da537427bd9f225fa068116e9db
Andy Wu <awpats12@gmail.com>
2023-03-20 00:25:40 -0400

    Added way to run both client and server on same shell

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,27 @@
+import path from 'path';
 import express from 'express';
 import cors from 'cors';
 
 import planetsRouter from './routes/planets/planets.router.js';
+
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
 app.use(cors({
   origin: 'http://localhost:3000'
 }));
+
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
 app.use(planetsRouter);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 export default app;
