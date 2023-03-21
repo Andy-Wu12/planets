9b4c411da9a6783775d1dcb86bb3dfad67c8bc89
Andy Wu <awpats12@gmail.com>
2023-03-21 19:45:44 -0400

    Logging with Morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 
 import planetsRouter from './routes/planets/planets.router.js';
 
@@ -14,7 +15,7 @@ const app = express();
 app.use(cors({
   origin: 'http://localhost:3000'
 }));
-
+app.use(morgan('combined'));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')))
