aa5d7a56591e49ff0e35e09ebb7dce4556b117b7
Andy Wu <awpats12@gmail.com>
2023-03-23 23:53:36 -0400

    Automated testing workflow set up, using supertest for http testing

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,8 +1,11 @@
+import request from 'supertest';
+
+import app from '../../app';
 
 describe('Test GET /launches', () => {
-  test('It should respond with 200 success', () => {
-    const response = 200;
-    expect(response).toBe(200);
+  test('It should respond with 200 success', async () => {
+    const response = await request(app).get('/launches');
+    expect(response.statusCode).toBe(200);
   });
 });
 
