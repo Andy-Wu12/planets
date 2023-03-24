43a418d4f91709cf0291e61c3772ed5bfda1081c
Andy Wu <awpats12@gmail.com>
2023-03-24 13:03:04 -0400

    Basic clustering in node

---
+++ clusters-lesson/server.js
@@ -0,0 +1,50 @@
+// https://nodejs.org/api/cluster.html
+
+import express from 'express';
+import cluster from 'cluster';
+
+const app = express();
+
+/* 
+  Imagine if you had the code below.
+  It causes performance issues if you trigger the delay, then trigger another request.
+  We can resolve these issues if we use CLUSTERS...
+*/
+function delay(duration) {
+  const startTime = Date.now();
+  while(Date.now() - startTime < duration) {
+    //event loop is blocked...
+  }
+}
+
+app.get('/', (req, res) => {
+  res.send(`Performance example : ${process.pid}`);
+});
+
+app.get('/timer', (req, res) => {
+  delay(9000);
+  res.send(`Ding ding ding! : ${process.pid}`);
+})
+
+/*
+  Running node server.js starts the master (or 'primary') process.
+  Each call to fork() creates a new worker process.
+  
+  Worker processes share requests with a 'round-robin' approach.
+  Requests are distributed by getting the first available one.
+  If the final worker is busy, 
+    the next request starts back at the first worker.
+
+  i.e. - If two different timer requests are triggered on both workers below,
+      another request to root will have to wait for first fork to unblock
+*/
+
+console.log("Running server.js");
+if(cluster.isPrimary) {
+  console.log('Master has been started...');
+  cluster.fork();
+  cluster.fork();
+} else {
+  console.log('Worker process started...');
+  app.listen(3000);
+}
