4e6e3eda1b01853d51ea0d51e8945c6f17eb74cc
Andy Wu <awpats12@gmail.com>
2023-03-25 14:56:59 -0400

    Set up mongoose and launches schema

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,28 @@
+import dotenv from 'dotenv';
 import http from 'http';
 
-import app from './app.js';
+import mongoose from 'mongoose';
 
+import app from './app.js';
 import { loadPlanetsData } from './models/planets.model.js';
+
+dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
+mongoose.connection.on('open', () => {
+  console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(process.env.MONGO_URI);
+
   await loadPlanetsData();
 
   server.listen(PORT, () => {
