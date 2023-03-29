79c2eb38cdee631db24c70ce5138a994406cc8d5
Andy Wu <awpats12@gmail.com>
2023-03-29 18:20:53 -0400

    Updated server structure to better support versioning

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,8 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 
-import planetsRouter from './routes/planets/planets.router';
-import launchesRouter from './routes/launches/launches.router';
+import api from './routes/api';
 
 const app = express();
 
@@ -16,8 +15,7 @@ app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')))
 
-app.use('/planets', planetsRouter);
-app.use('/launches', launchesRouter);
+app.use('/v1', api);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
