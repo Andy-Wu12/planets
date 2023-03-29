12e0853eb7e1e496bed80046ada218c49699d971
Andy Wu <awpats12@gmail.com>
2023-03-28 22:25:02 -0400

    Separated mongoose connection code into it's own module for testing

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,9 @@
 import dotenv from 'dotenv';
 import http from 'http';
 
-import mongoose from 'mongoose';
-
 import app from './app';
+
+import { mongoConnect } from './services/mongo';
 import { loadPlanetsData } from './models/planets.model';
 
 dotenv.config();
@@ -12,19 +12,9 @@ const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-mongoose.connection.on('open', () => {
-  console.log('MongoDB connection ready!');
-});
-
-mongoose.connection.on('error', (err) => {
-  console.error(err);
-});
 
 async function startServer() {
-  await mongoose.connect(process.env.MONGO_URI!, {
-    dbName: 'nasa-api'
-  });
-
+  await mongoConnect();
   await loadPlanetsData();
 
   server.listen(PORT, () => {
